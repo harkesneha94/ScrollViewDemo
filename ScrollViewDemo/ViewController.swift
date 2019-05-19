757ccf6338022e87594f6224b7d6256a6fcf8458
Sneha Harke <sneha.harke@ril.com>
2019-05-20 01:02:10 +0530

    Initial Commit

---
+++ ScrollViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollViewDemo
+//
+//  Created by Sneha Harke on 20/05/19.
+//  Copyright © 2019 Sneha Harke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ScrollViewDemoTests/ScrollViewDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ScrollViewDemoTests.swift
+//  ScrollViewDemoTests
+//
+//  Created by Sneha Harke on 20/05/19.
+//  Copyright © 2019 Sneha Harke. All rights reserved.
+//
+
+import XCTest
+@testable import ScrollViewDemo
+
+class ScrollViewDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
